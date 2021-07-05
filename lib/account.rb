78763940944f5599fe2df4f4ebb1a386dcfd09a3
JP Ferreira <jpferreira@MA301.broadband>
2021-07-05 12:02:21 +0100

    User can deposit money into account

--- lib/account.rb
+++ lib/account.rb
@@ -1,4 +1,15 @@
-
 class Account
+
+  attr_accessor :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def deposit(date, deposit_amount)
+    @deposit_amount = deposit_amount
+    @date = date
+    @balance = @deposit_amount
+  end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,5 +2,15 @@ require 'account'
 
 describe Account do
   
+  it 'can create a new account' do
+    @account = Account.new
+    expect(@account).to be_instance_of(Account)
+  end
+
+  it 'increases the balance available when a deposit is made' do
+    @account = Account.new
+    @account.deposit('01/06/2021', 1000)
+    expect(@account.balance).to eq(1000)
+  end
 
 end
