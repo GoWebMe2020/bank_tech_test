261fd6b82c1cc815cfc059b98ddc81870223d735
JP Ferreira <jpferreira@MA301.broadband>
2021-07-05 12:04:21 +0100

    Refactoring test to create new account before each test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,14 +1,16 @@
 require 'account'
 
 describe Account do
+
+  before :each do
+    @account = Account.new
+  end
   
   it 'can create a new account' do
-    @account = Account.new
     expect(@account).to be_instance_of(Account)
   end
 
   it 'increases the balance available when a deposit is made' do
-    @account = Account.new
     @account.deposit('01/06/2021', 1000)
     expect(@account.balance).to eq(1000)
   end
